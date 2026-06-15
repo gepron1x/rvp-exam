efbe040fd59400e6c3eeea2be265f069bb4519bf
gepron1x <georgijpronuk@gmail.com>
2026-06-15 11:13:21 +0300

    include remember me

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -21,10 +21,11 @@ def login():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
+        remember = request.form.get('remember') == 'on'
         if login and password:
             user = db.session.execute(db.select(User).filter_by(login=login)).scalar()
             if user and user.check_password(password):
-                login_user(user)
+                login_user(user, remember=remember)
                 flash('Вы успешно аутентифицированы.', 'success')
                 next = request.args.get('next')
                 return redirect(next or url_for('main.index'))
